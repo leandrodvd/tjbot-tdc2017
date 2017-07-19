3fa7a1ef69cf05cb4e38bd3e6bb525ced82f6885
Leandro David <leandrodvd@gmail.com>
2017-07-19 02:17:54 -0300

    add weather

--- tjbot.js
+++ tjbot.js
@@ -1,5 +1,6 @@
 const TJBot = require('tjbot');
 const led = require('./lib/led.js');
+const request = require('request');
 
 console.log("Default configuration:" )
 console.log(TJBot.prototype.defaultConfiguration);
@@ -76,16 +77,35 @@ tj.listen(function(msg) {
       tj.pauseListening();
       var speakPromise = tj.speak(response.description);
       if (speakPromise != undefined){
-	speakPromise.then(function(){
-	executeAction(response);
-	tj.resumeListening()} );
+      	speakPromise.then(function(){
+      	executeAction(response);
+      	tj.resumeListening()} );
       }
-	else{
-	 console.log("speak promise is undefined");
-	 tj.resumeListening();
-	}
+      else{
+    	 console.log("speak promise is undefined");
+    	 tj.resumeListening();
+    	}
     });
 });
+
+function getWeather(callback){
+  const base_url = credentials.weather.url;
+  const lat = credentials.weather.lat;
+  const long = credentials.weather.long;
+  const url = base_url+ "/api/weather/v1/geocode/"+ lat + "/"+ long + "/observations.json?language=pt-BR&units=m";
+
+  request(url, function(error, response, body) {
+    var message = "";
+    if(error){
+      message = "Não consegui acessar o serviço de clima";
+    }
+    else{
+      var obs_json = JSON.parse(body)
+      message = "O tempo lá fora está " + obs_json.observation.wx_phrase + " e a temperatura é de " + obs_json.observation.temp + " graus celsius";
+    }
+    return callback(error,message);
+  });
+}
 
 function processResponse(response){
   if (response.object.intents && response.object.intents.length>0){
@@ -95,6 +115,14 @@ function processResponse(response){
       console.log("message replaced: "+message);
       response.object.output.text[0]=message;
       response.description=message;
+    }
+    if (intent == 'weather'){
+      getWeather(function(err,message){
+        if(err){
+          console.log(err);
+        }
+        tj.speak(message);
+      });
     }
   }
   return response;
@@ -127,4 +155,3 @@ function executeAction(response){
   }
   return response;
 }
-
